65dcca0e2aeb8151050c5e1ff1e20ea36378b3fd
20Yiju <79932335+20Yiju@users.noreply.github.com>
2022-07-14 16:14:55 +0900

    add comment

--- [프로그래머스] 하샤드 수/[C++] 하샤드 수.cpp
+++ [프로그래머스] 하샤드 수/[C++] 하샤드 수.cpp
@@ -8,14 +8,18 @@ bool solution(int x) {
     bool answer = true;
     string input;
 
+    // 각 문자를 string을 변환하여 하나씩 접근 하고자
     stringstream ss;
     ss << x;
     ss >> input;
 
+    // 각 자리 수의 합 구하기
     int result = 0;
     for(int i = 0; i < input.length(); i++) {
         result += input[i] - '0';
     }
+    
+    // 나누어 떨어진 다면 true 아니면 false
     if(x%result == 0){
         answer = true;
     }
