bb6ba5a226be64d891a08a888df6c2c735f049d2
20Yiju <79932335+20Yiju@users.noreply.github.com>
2022-07-14 16:21:17 +0900

    add comment

--- [BOJ 1267] 핸드폰 요금/[C++] 핸드폰 요금.cpp
+++ [BOJ 1267] 핸드폰 요금/[C++] 핸드폰 요금.cpp
@@ -6,28 +6,28 @@ int main(){
     
     int N;
     cin >> N;
-
-    int Yresult = 0;
-    int Mresult = 0;
     
-    int call[N];
+    int Yresult = 0;  // 영식 결과
+    int Mresult = 0;  // 민식 결과
+    
+    int call[N]; // 통화 내역 저장 배열
     for(int i = 0; i < N; i++){
         cin >> call[i];
     }
 
    for(int i = 0; i < N; i++){
 
-        int Ycall = call[i];
+        int Ycall = call[i]; 
         int Mcall = call[i];
         
 
         while(1){
-            Yresult += 10;
-            if(Ycall < 30){
+            Yresult += 10; // 일단 반복문이 돌아간다는 뜻은 요금이 추가되어야한다는 뜻이므로 요금 부과
+            if(Ycall < 30){ // 30 보다 작은상태라면 위에서 요금이 추가된 이후에 더 이상 요금 부과가 없어도 되므로 반복문 종료
                 break;
             }
             else{
-                Ycall -= 30;
+                Ycall -= 30; // 30보다 큰 상태라면 요금 부과가 30 단위이므로 빼기를 해줌
             }
             
         }
@@ -42,14 +42,14 @@ int main(){
         }
     }
 
-    if(Yresult < Mresult) {
+    if(Yresult < Mresult) { // 민식이의 요금이 더 많다면 영식이 결과 보여주기
         cout << "Y " << Yresult;
     }
-    else if(Yresult > Mresult) {
+    else if(Yresult > Mresult) {  // 영식이의 요금이 더 많다면 민식이 결과 보여주기
         cout << "M " << Mresult;
     }
-    else{
-        cout << "Y M " << Mresult;
+    else{  // 영식, 민식이의 결과가 같다면 Y M 둘 다 출력
+        cout << "Y M " << Mresult; 
     }
 
     return 0;
