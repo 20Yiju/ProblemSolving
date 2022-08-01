c4c571050a9fbdbf5c1fa2c1707365580f434a34
20Yiju <79932335+20Yiju@users.noreply.github.com>
2022-08-01 15:28:08 +0900

    add comment

--- [BOJ 17211] 좋은 날 싫은 날/[Python] 좋은 날 싫은 날.py
+++ [BOJ 17211] 좋은 날 싫은 날/[Python] 좋은 날 싫은 날.py
@@ -9,6 +9,7 @@ else:
     gpercent = bg
     bpercent = bb
 
+# gpercent 의 값에 재현이의 기분이 좋은 날일 확률을 계산하여 업데이트해주었고 bpercent 의 값에 재현이의 기분이 싫은 날일 확률을 계산하여 업데이트
 
 for i in range (count - 1):
     g = gpercent
@@ -16,7 +17,8 @@ for i in range (count - 1):
 
     gpercent = (g * gg) + (b * bg)
     bpercent = (g * gb) + (b * bb)
-
+# Line 5 ~ Line 10 까지 처음 재현이의 기분에 따라 값을 gpercent 값과 bpercent 값을 설정 했기 때문에 Line 14 의 반복문은 전체 N 값에서 -1 한 만큼으로 범위를 설정
+    
 print(round(gpercent * 1000))
 print(round(bpercent * 1000))
 
