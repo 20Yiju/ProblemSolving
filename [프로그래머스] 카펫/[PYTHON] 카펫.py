8c334356f50fc7643d1b30dd92d2bee3b476f2bb
Yiju Jung <79932335+20Yiju@users.noreply.github.com>
2023-08-13 01:28:24 +0900

    Update [PYTHON] 카펫.py

--- [프로그래머스] 카펫/[PYTHON] 카펫.py
+++ [프로그래머스] 카펫/[PYTHON] 카펫.py
@@ -4,9 +4,9 @@ def solution(brown, yellow):
     total = brown + yellow
     
     for h in range(3, total):
-        w = total // h
+        w = total // h    #total = w * s = brown + yellow
         
-        if w * h == total and w >= h and brown + 4 == (2*h) + (2*w):
+        if w * h == total and w >= h and brown + 4 == (2*h) + (2*w): #total = b + y = h * w, yellow = (w - 2) * (h - 2)에 대입해서 풀었음
             answer.append(w)
             answer.append(h)
             
